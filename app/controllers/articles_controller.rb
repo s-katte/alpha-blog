b94fbe5a43de446ce972d62e9a014ff57b050b5c
s-katte <shubhamkatte99@gmail.com>
2021-06-10 15:33:48 +0530

    Create show action and add gravatar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.find(3)
     if @article.save
       flash[:success] = "Article was successfuly created"
       redirect_to article_path(@article)
