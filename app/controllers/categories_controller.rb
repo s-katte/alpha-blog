a1a7e568848421a66c8fcb8776b7ae331325d39d
s-katte <shubhamkatte99@gmail.com>
2021-06-11 17:09:39 +0530

    Add admin required to create cats

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -25,6 +26,14 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = "Only admin can to do this action"
+      redirect_to categories_path
+    end
+
   end
 
 end
